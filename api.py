ffff6b7a2156a0eac4cc1be7b6d794f2e7d61619
siguaHuang <lu981300@gmail.com>
2019-08-02 22:36:43 +0800

    gateway API add

---
+++ api.py
@@ -0,0 +1,18 @@
+import flask
+import urllib.request
+from flask import jsonify, request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+
+
+@app.route('/', methods=['POST'])
+def home():
+    content=request.json
+    print(content['url'])
+    filename = content['name'] + '_' + content['version']
+    urllib.request.urlretrieve(content['url'], filename)
+    return "OK!\n"
+    
+app.run()
